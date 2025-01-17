9eed614afb8039ccb21e20190de417501c849833
Louis-Nicolas Longheval <louisnicolas.longheval@student.vinci.be>
2025-01-17 17:04:30 +0100

    test: add special character validation test (Red phase)

--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -4,4 +4,8 @@ describe('Password Validation', () => {
   it('should reject passwords shorter than 8 characters', () => {
     expect(isValidPassword('Abc1!@')).toBe(false);
   });
+
+  it('should reject passwords without special characters', () => {
+    expect(isValidPassword('Abcd1234')).toBe(false);
+  });
 }); 
